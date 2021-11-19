8550438ff18cd547d9ddc4961dab4e7bfadeaaf4
Marcel Buian <marcel.buian@runitonce.com>
2021-11-19 23:43:50 +0100

    Add Subtractor

---
+++ app/Services/Subtractor.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Services;
+
+use App\Exceptions\OutOfBoundException;
+use App\Models\Number;
+
+class Subtractor implements Calculator
+{
+    public function operation(): string
+    {
+        return 'minus';
+    }
+
+    /**
+     * @throws OutOfBoundException
+     */
+    public function calculate(Number $a, Number $b): Number
+    {
+        $result = $a->get() - $b->get();
+        if (abs($result) > config('calculator.max_supported_value')) {
+            throw OutOfBoundException::fromNumbers($this, $a, $b);
+        }
+
+        return new Number($result);
+    }
+}
--- tests/Unit/SubtractorTest.php
+++ tests/Unit/SubtractorTest.php
@@ -4,30 +4,30 @@ namespace Tests\Unit;
 
 use App\Exceptions\OutOfBoundException;
 use App\Models\Number;
-use App\Services\Adder;
+use App\Services\Subtractor;
 use Tests\TestCase;
 
-class AdderTest extends TestCase
+class SubtractorTest extends TestCase
 {
     public function test_adder_operation()
     {
-        $this->assertSame('plus', (new Adder)->operation());
+        $this->assertSame('minus', (new Subtractor)->operation());
     }
 
     public function test_adder_calculates_well()
     {
-        $adder = new Adder;
+        $adder = new Subtractor;
         $result = $adder->calculate(new Number(-2.5), new Number(3.5));
 
-        $this->assertSame(1.0, $result->get());
+        $this->assertSame(-6.0, $result->get());
     }
 
     public function test_adder_returns_out_of_bound_exception()
     {
         $this->expectException(OutOfBoundException::class);
-        $this->expectExceptionMessage('9 plus 2 exceeds the maximum allowed value of 10');
+        $this->expectExceptionMessage('-9 minus 2 exceeds the maximum allowed value of 10');
 
         config(['calculator.max_supported_value' => 10]);
-        (new Adder)->calculate(new Number(9), new Number(2));
+        (new Subtractor)->calculate(new Number(-9), new Number(2));
     }
 }
